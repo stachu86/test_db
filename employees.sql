39f5582221d237fa25fe7d45981ad39bbabc1f31
Stanisław Kacprzak <skacprza@agh.edu.pl>
2022-10-06 18:55:39 +0200

    set to_date to NULL for employees that still work in the company

--- employees.sql
+++ employees.sql
@@ -59,7 +59,7 @@ CREATE TABLE dept_manager (
    emp_no       INT             NOT NULL,
    dept_no      CHAR(4)         NOT NULL,
    from_date    DATE            NOT NULL,
-   to_date      DATE            NOT NULL,
+   to_date      DATE,
    FOREIGN KEY (emp_no)  REFERENCES employees (emp_no)    ON DELETE CASCADE,
    FOREIGN KEY (dept_no) REFERENCES departments (dept_no) ON DELETE CASCADE,
    PRIMARY KEY (emp_no,dept_no)
@@ -69,7 +69,7 @@ CREATE TABLE dept_emp (
     emp_no      INT             NOT NULL,
     dept_no     CHAR(4)         NOT NULL,
     from_date   DATE            NOT NULL,
-    to_date     DATE            NOT NULL,
+    to_date     DATE,
     FOREIGN KEY (emp_no)  REFERENCES employees   (emp_no)  ON DELETE CASCADE,
     FOREIGN KEY (dept_no) REFERENCES departments (dept_no) ON DELETE CASCADE,
     PRIMARY KEY (emp_no,dept_no)
@@ -89,7 +89,7 @@ CREATE TABLE salaries (
     emp_no      INT             NOT NULL,
     salary      INT             NOT NULL,
     from_date   DATE            NOT NULL,
-    to_date     DATE            NOT NULL,
+    to_date     DATE,
     FOREIGN KEY (emp_no) REFERENCES employees (emp_no) ON DELETE CASCADE,
     PRIMARY KEY (emp_no, from_date)
 ) 
